7e74c985142629a37a5936bdd8e58572d7d3600e
rodrigo-projects <rodrigorrsousa6@gmail.com>
2021-10-12 23:56:16 -0300

    iconsDetails

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import { Layout, Menu, Breadcrumb } from "antd";
 import "./home.css";
-import { CarOutlined, UnlockOutlined } from '@ant-design/icons';
+import { CarOutlined,DeleteRowOutlined, FireOutlined, 
+  NotificationOutlined,UnlockOutlined,ThunderboltOutlined,
+  EnvironmentOutlined, BulbOutlined } from "@ant-design/icons";
 
 const { Header, Content, Footer } = Layout;
 function Home() {
@@ -8,9 +10,11 @@ function Home() {
     <Layout>
       <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
           <Menu.Item key="1">Home</Menu.Item>
-          <Menu.Item key="2">Monitoramento (Em breve...)</Menu.Item>
+          <Menu.Item disabled key="2">
+            Monitoramento (Em breve...)
+          </Menu.Item>
         </Menu>
       </Header>
       <Content
@@ -25,17 +29,22 @@ function Home() {
           style={{ padding: 24, minHeight: 380 }}
         >
           <div className="home">
-            <h1 className="text"><UnlockOutlined /> TranCar <CarOutlined/></h1>   
+            <h1 className="text">
+              <UnlockOutlined /> TranCar <CarOutlined />
+            </h1>
             <h2>Tenha os dados do seu carro na palma da sua mão:</h2>
-            <li>Localização em tempo real</li>
-            <li>Nível do tanque</li>
-            <li>Aberto/fechado</li>
-            
+            <h4><EnvironmentOutlined /> Localização em tempo real </h4>
+            <h4><DeleteRowOutlined /> Nível do tanque</h4>
+            <h4><ThunderboltOutlined /> Nível de bateria </h4>
+            <h4><NotificationOutlined /> Ligado/desligado (alarme)</h4>
+            <h4><FireOutlined /> Temperatura</h4>
+            <h4><BulbOutlined /> Novas funcionalidades em breve...</h4>
           </div>
         </div>
       </Content>
       <Footer style={{ textAlign: "center" }}>
-        Autores: Rodrigo - Bruno | IFMG - Betim
+        <strong> Autores: Rodrigo - Bruno | IFMG - Betim </strong>
+        <p> Beta 0.1</p>
       </Footer>
     </Layout>
   );
